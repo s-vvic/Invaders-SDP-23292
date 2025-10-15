0a959b498f1ccdc73780b3a9ea4a0a2e7a527e69
s0nh <wlq8112@hanyang.ac.kr>
2025-10-15 22:50:57 +0900

    refactor SoundButton class

--- src/screen/TitleScreen.java
+++ src/screen/TitleScreen.java
@@ -79,7 +79,10 @@ public class TitleScreen extends Screen {
 					this.isRunning = false;
 				} else {
 					this.soundButton.changeSoundState();
-					// TODO : Sound setting.
+
+					if (SoundButton.getIsSoundOn()) {
+						// TODO : Sound setting.
+						}
 
 					this.selectionCooldown.reset();
 				}
@@ -143,13 +146,5 @@ public class TitleScreen extends Screen {
 				this.height * 4 / 5 - 16);
 
 		drawManager.completeDrawing(this);
-	}
-
-	/**
-	 * Getter for the sound state.
-	 * @return isSoundOn of the sound button.
-	 */
-	public boolean getIsSoundOn() {
-		return this.soundButton.getIsSoundOn();
 	}
 }
