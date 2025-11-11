a876ec419c332cee10f2c5024c3f846b103f2c24
s0nh <115431904+s0nh@users.noreply.github.com>
2025-11-12 06:31:26 +0900

    Revert "Revert "Sdp 110 add dash pattern""

--- src/entity/FinalBoss.java
+++ src/entity/FinalBoss.java
@@ -16,10 +16,29 @@ public class FinalBoss extends Entity implements BossEntity{
     private int maxHp;
     private final int pointValue;
     private boolean isDestroyed;
+
+    /** The Ship of player */
+    private Ship player;
+    /** The Fixed Position X of player ship when start dash pattern*/
+    private int fixedPositionX;
+    /** The Fixed Position Y of player ship when start dash pattern*/
+    private int fixedPositionY;
     /** for move pattern */
     private int zigDirection = 1;
     /** for move pattern */
     private boolean goingDown = true;
+    /** Dash Velocity X */
+    private double dashVelX = 0;
+    /** Dash Velocity Y */
+    private double dashVelY = 0;
+    /** The Decimal Point of dashVelX */
+    private double subDashVelX;
+    /** The Decimal Point of dashVelY */
+    private double subDashVelY;
+    /** Degree of shaking X axis */
+    private int shakeOffX = 0;
+    /** Degree of shaking Y axis */
+    private int shakeOffY = 0;
 
     private Cooldown animationCooldown;
     /** Shoot1's cool down */
@@ -28,11 +47,26 @@ public class FinalBoss extends Entity implements BossEntity{
     private Cooldown shootCooldown2;
     /** Shoot3's cool down */
     private Cooldown shootCooldown3;
+    /** charging's cool down */
+    private Cooldown chargeCooldown;
+    /** Dash's cool down */
+    private Cooldown dashCooldown;
+
+    /** Dash Pattern State */
+    private enum DashPattern {
+        FOLLOWING,
+        CHARGING,
+        DASH,
+    }
+
+    /** Dash Pattern Current State */
+    private DashPattern dashOption;
+
     private int screenWidth;
     private int screenHeight;
     /** basic attribute of final boss */
 
-    public FinalBoss(int positionX, int positionY, int screenWidth, int screenHeight){
+    public FinalBoss(int positionX, int positionY, int screenWidth, int screenHeight, Ship ship){
 
         super(positionX,positionY,100,80, Color.RED);
         if (GameState.isDecreaseEnemyPower()) {
@@ -46,11 +80,15 @@ public class FinalBoss extends Entity implements BossEntity{
         this.isDestroyed = false;
         this.screenWidth = screenWidth;
         this.screenHeight = screenHeight;
+        this.player = ship;
+        this.dashOption = DashPattern.FOLLOWING;
 
         this.animationCooldown = Core.getCooldown(500);
         this.shootCooldown1 = Core.getCooldown(5000);
         this.shootCooldown2 = Core.getCooldown(400);
         this.shootCooldown3 = Core.getCooldown(300);
+        this.chargeCooldown = Core.getCooldown(2000);
+        this.dashCooldown = Core.getCooldown(6000);
 
     }
 
@@ -109,13 +147,34 @@ public class FinalBoss extends Entity implements BossEntity{
         return this.pointValue;
     }
 
+    /** move simple */
+    @Override
+    public void move(int distanceX, int distanceY){
+        this.positionX += distanceX;
+        this.positionY += distanceY;
+    }
+    
     /** movement pattern of final boss */
     public void movePattern(){
-        if(this.healPoint > this.maxHp/2){
+        if(this.healPoint > this.maxHp*3/4){
             this.move(0,0);
         }
-        else if (this.healPoint > this.maxHp/4){
+        else if (this.healPoint > this.maxHp/2){
             this.moveZigzag(4,3);
+        } 
+        else if (this.healPoint > this.maxHp/4) {
+            switch (this.dashOption) {
+                case FOLLOWING:
+                    this.follow(10);
+                    this.chargeCooldown.reset();
+                    break;
+                case CHARGING:
+                    this.charge();
+                    break;
+                case DASH:
+                    dash();
+                    break;
+            }
         }
         else {
             this.moveZigzag(2,1);
@@ -139,14 +198,74 @@ public class FinalBoss extends Entity implements BossEntity{
         }
     }
 
-    /** move simple */
-    @Override
-    public void move(int distanceX, int distanceY){
-        this.positionX += distanceX;
-        this.positionY += distanceY;
+    /** Fix the Position of Player Ship */
+    public void follow(int dashSpeed) {
+        this.dashCooldown.reset();
+        this.fixedPositionX = this.player.getPositionX();
+        this.fixedPositionY = this.player.getPositionY();
+
+        double angle = Math.atan2(fixedPositionY - positionY, fixedPositionX - positionX);
+
+        this.dashVelX = dashSpeed * Math.cos(angle);
+        this.dashVelY = dashSpeed * Math.sin(angle);
+        this.dashOption = DashPattern.CHARGING;
+
+        this.subDashVelX = 0;
+        this.subDashVelY = 0;
     }
 
-    /** shooting pattern of final boss */
+    /** Charge for a While Before Starting Dash Pattern */
+    public void charge() {
+        if (this.chargeCooldown.checkFinished()) {
+            this.dashOption = DashPattern.DASH;
+
+            this.move(-shakeOffX, -shakeOffY);
+            this.shakeOffX = 0;
+            this.shakeOffY = 0;
+
+        } else {
+
+            this.move(-shakeOffX, -shakeOffY);
+
+            int shakeAmount = 2;
+            this.shakeOffX = (((int) (Math.random() * 3)) - 1) * shakeAmount;
+            this.shakeOffY = (((int) (Math.random() * 3)) - 1) * shakeAmount;
+
+            this.move(this.shakeOffX, this.shakeOffY);
+        }
+    }
+
+    /** Dash Pattern */
+    public void dash() {
+        double distance = Math.sqrt((fixedPositionX-positionX) * (fixedPositionX-positionX) + (fixedPositionY-positionY) * (fixedPositionY-positionY));
+        double currentSpeed = Math.sqrt(dashVelX*dashVelX + dashVelY*dashVelY);
+
+        if (distance <= currentSpeed || currentSpeed == 0) {
+            this.positionX = this.fixedPositionX;
+            this.positionY = this.fixedPositionY;
+
+            if (this.dashCooldown.checkFinished()) {
+                this.dashCooldown.reset();
+                this.dashOption = DashPattern.FOLLOWING;
+                this.dashVelX = 0;
+                this.dashVelY = 0;
+            }
+        } else {
+
+            this.subDashVelX += this.dashVelX;
+            this.subDashVelY += this.dashVelY;
+
+            int moveX = (int) this.subDashVelX;
+            int moveY = (int) this.subDashVelY;
+
+            if (moveX != 0 || moveY !=0) {
+                this.move(moveX, moveY);
+
+                this.subDashVelX -= moveX;
+                this.subDashVelY -= moveY;
+            }
+        }
+    }
 
 
     /** first shooting pattern of final boss */
@@ -180,18 +299,15 @@ public class FinalBoss extends Entity implements BossEntity{
         Set<BossBullet> bullets = new HashSet<>();
         if (this.shootCooldown3.checkFinished()) {
             this.shootCooldown3.reset();
-//            if (!(this.getPositionX() == 0 || this.getPositionX() == 400)){
+         // if (!(this.getPositionX() == 0 || this.getPositionX() == 400)){
                 BossBullet bullet1 = new BossBullet(this.getPositionX() + this.getWidth() / 2 - 3 + 70, this.positionY, 0, 5,6,10,Color.blue);
                 BossBullet bullet2 = new BossBullet(this.getPositionX() + this.getWidth() / 2 - 3 - 70, this.positionY, 0, 5,6,10,Color.blue);
                 bullets.add(bullet1);
                 bullets.add(bullet2);
-//            }
+         // }
         }
         return bullets;
     }
-
-
-
 
 
     /** flag final boss' destroy */
